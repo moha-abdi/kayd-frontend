cac57cff4c85dfa8c9a3ed2fcfb97970511308d1
moha-abdi <mohaa6052@gmail.com>
2025-01-21 07:06:37 +0300

    Add root layout component with authentication handling and first launch check

---
+++ app/_layout.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect } from "react";
+import { Slot, useRouter, useSegments } from "expo-router";
+import { AuthProvider, useAuth } from "../contexts/AuthContext";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import "@/global.css";
+import { StatusBar } from "expo-status-bar";
+
+function RootLayoutNav() {
+  const { user, loading } = useAuth();
+  const segments = useSegments();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!loading) {
+      const checkFirstLaunch = async () => {
+        const hasLaunched = await AsyncStorage.getItem("hasLaunched");
+        if (!hasLaunched) {
+          await AsyncStorage.setItem("hasLaunched", "true");
+          router.replace("/onboarding");
+        } else {
+          const inAuthGroup = segments[0] === "(auth)";
+          if (!user && !inAuthGroup) {
+            router.replace("/login");
+          } else if (user && inAuthGroup) {
+            router.replace("/");
+          }
+        }
+      };
+
+      checkFirstLaunch();
+    }
+  }, [user, loading]);
+
+  if (loading) {
+    return <Slot />;
+  }
+
+  return <Slot />;
+}
+
+export default function RootLayout() {
+  return (
+    <>
+    <StatusBar style="auto" />
+    <AuthProvider>
+      <RootLayoutNav />
+    </AuthProvider>
+    </>
+  );
+}
